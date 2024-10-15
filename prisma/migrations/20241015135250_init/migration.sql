75fd2cadb8a147e5e835aed063aa5f256e010e23
“seiha-tsuge” <seiha.tsuge@gmail.com>
2024-10-15 22:53:37 +0900

    refactor: DBのテーブル名を小文字に変更

--- prisma/migrations/20241015135250_init/migration.sql
+++ prisma/migrations/20241015135250_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "Task" (
+CREATE TABLE "task" (
     "task_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
@@ -7,52 +7,52 @@ CREATE TABLE "Task" (
     "order" INTEGER NOT NULL,
     "category_id" UUID NOT NULL,
 
-    CONSTRAINT "Task_pkey" PRIMARY KEY ("task_id")
+    CONSTRAINT "task_pkey" PRIMARY KEY ("task_id")
 );
 
 -- CreateTable
-CREATE TABLE "Category" (
+CREATE TABLE "category" (
     "category_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
 
-    CONSTRAINT "Category_pkey" PRIMARY KEY ("category_id")
+    CONSTRAINT "category_pkey" PRIMARY KEY ("category_id")
 );
 
 -- CreateTable
-CREATE TABLE "Member" (
+CREATE TABLE "member" (
     "member_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "enrollment_status" TEXT NOT NULL,
     "join_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Member_pkey" PRIMARY KEY ("member_id")
+    CONSTRAINT "member_pkey" PRIMARY KEY ("member_id")
 );
 
 -- CreateTable
-CREATE TABLE "Pair" (
+CREATE TABLE "pair" (
     "pair_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "pair_code" TEXT NOT NULL,
     "pair_name" TEXT NOT NULL,
     "team_id" UUID NOT NULL,
 
-    CONSTRAINT "Pair_pkey" PRIMARY KEY ("pair_id")
+    CONSTRAINT "pair_pkey" PRIMARY KEY ("pair_id")
 );
 
 -- CreateTable
-CREATE TABLE "Team" (
+CREATE TABLE "team" (
     "team_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "team_code" TEXT NOT NULL,
     "team_name" TEXT NOT NULL,
 
-    CONSTRAINT "Team_pkey" PRIMARY KEY ("team_id")
+    CONSTRAINT "team_pkey" PRIMARY KEY ("team_id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+CREATE UNIQUE INDEX "category_name_key" ON "category"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Member_email_key" ON "Member"("email");
+CREATE UNIQUE INDEX "member_email_key" ON "member"("email");
 
 -- AddForeignKey
-ALTER TABLE "Task" ADD CONSTRAINT "Task_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("category_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "task" ADD CONSTRAINT "task_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "category"("category_id") ON DELETE RESTRICT ON UPDATE CASCADE;
